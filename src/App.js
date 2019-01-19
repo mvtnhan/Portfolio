473ff2ec81e706a740655b3ed8b8a7d1306350a9
Nhan Mai <mvtnhan@gmail.com>
2019-01-19 13:18:48 +0700

    review and update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
-//import Shopify from "./landing-pages/shopify/index.js";
-import Craftsman from "./landing-pages/craftsman/index.js";
+import Shopify from "./landing-pages/shopify/index.js";
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/" component={Craftsman} />
+        <Route path="/" component={Shopify} />
       </Router>
     );
   }
--- src/landing-pages/shopify/index.js
+++ src/landing-pages/shopify/index.js
@@ -15,26 +15,24 @@ export function renderHeader() {
       </a>
       <h1 className="MainTitle">Sell online with Shopify</h1>
       <h3 className="SubTitle">Trusted by over 600,000 businesses worldwide</h3>
-      <div className="BoxEmail">
-        <div className="Email">
-          <input
-            className="EnterEmail"
-            type="text"
-            placeholder="Enter your email address"
-          />
-          <button className="Button ButtonStart">Start free trial</button>
-        </div>
-        <p>
-          Try Shopify free for 14 days. No risk, and no credit card required
-        </p>
+      <div className="SubscribeEmail">
+        <input
+          type="text"
+          className="InputText"
+          placeholder="Enter your email address"
+        />
+        <button className="Button ButtonSecondary">Start free trial</button>
       </div>
+      <p className="TryFree">
+        Try Shopify free for 14 days. No risk, and no credit card required
+      </p>
       <img className="Laptop" src={laptop} alt="laptop" />
     </div>
   );
 }
 export function renderFeatures() {
   return (
-    <div className="Features">
+    <div className="Features Container">
       <div className="BoxFeatures">
         <SvgFeatureThemes />
         <h3>Beautiful themes that are responsive and customizable</h3>
@@ -67,30 +65,32 @@ export function renderFeatures() {
 }
 export function renderNewspapers() {
   return (
-    <div className="Newspapers">
+    <div className="Newspapers Container">
       <img src={spapers} alt="new spapers" />
     </div>
   );
 }
 export function renderQuotes() {
   return (
-    <div className="Quotes">
+    <div className="Quotes Container">
       <h3>
         "Shopify is better than any other platform we've played with, and we've
         played with them all"
       </h3>
       <p>JONATHON BAYME, CEO OF THEORY11</p>
-      <button className="Button ButtonBottom">Start free trial</button>
+      <button className="Button ButtonPrimary">Start free trial</button>
     </div>
   );
 }
 export function renderFooter() {
+  /* eslint-disable jsx-a11y/anchor-is-valid */
   return (
     <div className="Footer">
       <a href="#">Terms of Service</a>
       <a href="#">Privacy Policy</a>
     </div>
   );
+  /* eslint-enable jsx-a11y/anchor-is-valid */
 }
 export default function Shopify() {
   return (
