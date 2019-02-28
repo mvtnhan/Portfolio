6a3269f7e6054a064d19ba0792d8a9eabe0ce701
Nhan Mai <mvtnhan@gmail.com>
2019-02-28 08:32:41 +0700

    Update response

--- src/landing-pages/craftsman/index.js
+++ src/landing-pages/craftsman/index.js
@@ -1,148 +1,137 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
 import React from "react";
 import { Image, Popup } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "./scraftsman.scss";
 
-import Logo from "./images/logo.png";
+import Logo from "./images/Logo.png";
 import LestHost from "./images/LetsHost.png";
-import FFVentureCapital from "./images/ffVentureCapital.png";
+import FFVentureCapital from "./images/FFVentureCapital.png";
 import RavenDB from "./images/RavenDB.png";
 import CloudVPS from "./images/CloudVPS.png";
 import Host365 from "./images/Host365.png";
 import MedievalWall from "./images/MedievalWall.png";
-import About from "./images/about.png";
-import Skills from "./images/skills.png";
+import About from "./images/About.png";
+import Contact from "./images/Contact.png";
+import Skills from "./images/Skills.png";
 
-//import SmallIntro from "./images/intro-small.png";
+export const Social = {
+  linkedin: "https://www.linkedin.com/in/mvtnhan/",
+  twitter: "https://twitter.com/mvtnhan",
+  email: "mailto:someone@example.com",
+  skype: "https://join.skype.com/invite/c2MTvJakdVJG"
+};
 
 export function renderHeader() {
   return (
-    <div className="Header PageContainerScrafts">
+    <header className="Header Container">
       <h1 className="Logo">
         <img src={Logo} alt="Logo Web Craftsman" />
       </h1>
-    </div>
+    </header>
   );
 }
 
 export function renderIntro() {
-  /* eslint-disable jsx-a11y/anchor-is-valid */
   return (
     <section className="Intro">
-      <div className="ImgIntro">
-        <a className="Linkedin" href="https://www.linkedin.com/in/mvtnhan/">
+      <div className="ImgScraftsman">
+        <a className="Linkedin" href={Social.Linkedin}>
           Linkedin
         </a>
-        <a className="Twitter" href="https://www.linkedin.com/in/mvtnhan/">
+        <a className="Twitter" href={Social.twitter}>
           Twitter
         </a>
-        <div className="AroundFindOutMe" />
-        <div className="FindOutMe">
+        <div className="AroundFindOutMore" />
+        <div className="FindOutMore">
           <Image href="#" className="ImgFindOutMe">
-            Find Out <p>More</p>
+            Find Out <span>More</span>
           </Image>
-          <a class="MyWorks" href="#">
-            Contact Info
-          </a>
-          <a class="AboutMe" href="#">
+          <a class=" MyWorks" href="#">
             My Works
           </a>
-          <a class="ContactInfo" href="#">
+          <a class="AboutMe" href="#">
             About Me
+          </a>
+          <a class="ContactInfo" href="#">
+            Contact Info
           </a>
         </div>
       </div>
       <h2>I'm Marijan, Amsterdam based UX Designer working with Booking.com</h2>
     </section>
   );
-  /* eslint-enable jsx-a11y/anchor-is-valid */
 }
 
-export function renderWorks() {
-  /* eslint-disable jsx-a11y/anchor-is-valid */
+export function renderMoreWorks() {
   return (
-    <section className="Works">
-      <div className="PageContainerScrafts">
-        <a href="#" className="Left-top">
+    <section className="MoreWorks">
+      <div className="Container">
+        <a href="#" className="LeftTop">
           <img src={LestHost} alt="LesHost" />
         </a>
-
-        <a href="#" className="Left-mid">
+        <a href="#" className="LeftMid">
           <img src={FFVentureCapital} alt="ff VentureCapital" />
         </a>
-
-        <a href="#" className="Left-bot">
+        <a href="#" className="LeftBot">
           <img src={RavenDB} alt="RavenDB" />
         </a>
-
-        <a href="#" className="Right-top">
+        <a href="#" className="RightTop">
           <img src={CloudVPS} alt="CloudVPS" />
         </a>
-
-        <a href="#" className="Right-mid">
+        <a href="#" className="RightMid">
           <img src={Host365} alt="Host365" />
         </a>
-
-        <a href="#" className="Right-bot">
+        <a href="#" className="RightBot">
           <img src={MedievalWall} alt="MedievalWall" />
         </a>
         <a href="#" className="ImgMoreWorks">
-          <em>More</em>Works
+          <em>More</em> Works
         </a>
-        <div className="CraftsmanWorks">
-          <div className="TextAboutWorks">
-            <span className="QuestionOpened">“</span>
-            On every project my goal is to create an functional and consistent
-            UX which serves its main purpose. I always check if every detail is
-            in the right place and implemented according to the latest web
-            standards.
-            <span className="QuestionClosed">”</span>
-          </div>
-        </div>
+        <span className="QuestionOpened">“</span>
+        <p>
+          On every project my goal is to create an functional and consistent UX
+          which serves its main purpose. I always check if every detail is in
+          the right place and implemented according to the latest web standards.
+        </p>
+        <span className="QuestionClosed">”</span>
       </div>
     </section>
   );
-  /* eslint-enable jsx-a11y/anchor-is-valid */
 }
 export function renderAbout() {
-  /* eslint-disable jsx-a11y/anchor-is-valid */
   return (
-    <div className="About">
-      <div className="PageContainerScrafts">
+    <div className="CraftmansPhoto">
+      <div className="Container">
         <div className="AboutSection">
-          <div className="AboutImg">
-            <a href="#" className="PhotoMore">
-              Craftsman <em>Photo</em>
-            </a>
-            <img src={About} alt="About" width="427" height="375" />
-          </div>
+          <Image src={About} alt="About" width="427" height="375" />
+          <a href="#" className="PhotoMore">
+            Craftsman <em>Photo</em>
+          </a>
           <article>
-            <header>
-              <h2>My Greetings & Handshake to You</h2>
-              <p>
-                Welcome to my little place on the internet. I’m Marijan, Web
-                Designer and UX professional working with{" "}
-                <a href="#">Booking.com</a> on world's #1 accomodation site.
-              </p>
-              <p>
-                Working within role of Web and UX Designer since 2006. On each
-                project I bring a diversified set of skills which have been
-                gathered from projects of all sizes, versatile design styles and
-                different markets.
-              </p>
-              <p>
-                I’m specialized in web design and development using handwritten
-                HTML, CSS and jQuery framework. Love working with WordPress,
-                also have considerable experience with other platforms as well.
-              </p>
-            </header>
+            <h2>My Greetings & Handshake to You</h2>
+            <p>
+              Welcome to my little place on the internet. I’m Marijan, Web
+              Designer and UX professional working with{" "}
+              <a href="#">Booking.com</a> on world's #1 accomodation site.
+            </p>
+            <p>
+              Working within role of Web and UX Designer since 2006. On each
+              project I bring a diversified set of skills which have been
+              gathered from projects of all sizes, versatile design styles and
+              different markets.
+            </p>
+            <p>
+              I’m specialized in web design and development using handwritten
+              HTML, CSS and jQuery framework. Love working with WordPress, also
+              have considerable experience with other platforms as well.
+            </p>
           </article>
         </div>
         <div className="AboutSection">
           <article className="HardAndPlay">
-            <header>
-              <h3>I Work Hard and Play Hard</h3>
-            </header>
+            <h3>I Work Hard and Play Hard </h3>
             <p>
               I consider stable communication and quick response important
               factors of project success. So when working together I think of me
@@ -156,94 +145,105 @@ export function renderAbout() {
             </p>
           </article>
           <div className="GreatUX">
-            <ul>
-              <li className="First">
-                Defined
-                <p>Purpose</p>
-              </li>
-              <li className="Second">
-                Unique
-                <p>Design</p>
-              </li>
-              <li className="Third">
-                Quality
-                <p>Code</p>
-              </li>
-
-              <strong>Efficient Web Presence</strong>
-            </ul>
+            <a href="#">
+              Defined<span>Purpose</span>
+            </a>
+            <a href="#">
+              Defined<span>Purpose</span>
+            </a>
+            <a href="#">
+              Defined<span>Purpose</span>
+            </a>
+            <strong>Efficient Web Presence</strong>
             <p className="Conclusion">GreatUX</p>
           </div>
         </div>
       </div>
     </div>
   );
-  /* eslint-enable jsx-a11y/anchor-is-valid */
 }
-
-const Sociallist = [
-  {
-    link: "http://mvtnhan@gmail.com",
-    img: "Email",
-    content: "mvtnhan@gmail.com"
-  },
-  {
-    link: "https://join.skype.com/invite/c2MTvJakdVJG",
-    img: "Skype",
-    content: "Skype"
-  },
-  {
-    link: "https://twitter.com/mvtnhan",
-    img: "Twitter",
-    content: "Twitter"
-  },
-  {
-    link: "https://www.linkedin.com/in/mvtnhan",
-    img: "Linkedin",
-    content: "Linkedin"
-  }
-];
 
 export function renderContact() {
   return (
-    <section className="Contact">
-      <div className="PageContainerScrafts">
-        <div className="ContainerFrom">
-          <form className="ContactForm">
-            <input type="text" name="Name" id="Name" placeholder="Name" />
-            <input type="text" name="E-mail" id="E-mail" placeholder="E-mail" />
-            <textarea
-              name="Message"
-              id="Message"
-              rows="9"
-              cols="26"
-              placeholder="How can I help you?"
-            />
-          </form>
-          <div className="SkillsImg">
-            <img src={Skills} alt="Skills" width="225" height="75" />
-          </div>
+    <section className="Contact ">
+      <div className="Container FromContact">
+        <form>
+          <input type="text" name="Name" id="Name" placeholder="Name" />
+          <input type="text" name="Email" id="Email" placeholder="Email" />
+          <textarea
+            name="Message"
+            id="Message"
+            rows="9"
+            cols="26"
+            placeholder="How can I help you?"
+          />
+        </form>
+        <Image
+          src={Contact}
+          alt="ContactMe"
+          className="ContactImg"
+          width="470"
+          height="400"
+        />
+        <div className="SkillsImg">
+          <Popup
+            className="TooltipCraftsman"
+            trigger={<Image src={Skills} width="225" height="75" />}
+            content="HTML5 Powered with CSS3 / Styling, Device Access, Performance & Integration, and Semantics"
+            position="bottom center"
+          />
+          {/* <img src={Skills} alt="Skills" width="225" height="75" /> */}
         </div>
-        <div className="Footer">
-          <p>Copyright © 2014 Web Craftsman. Contact Preferred Via:</p>
-          <div className="IconFooter">
-            {Sociallist.map(social => (
-              <>
-                {" "}
-                {/* eslint-disable-next-line */}
-                <a href={social.link} />
-                <Popup
-                  className="PopupoCraftsman"
-                  trigger={<Image className={social.img} />}
-                  content={social.content}
-                  position="top center"
-                />
-              </>
-            ))}
-          </div>
-        </div>
+        <a href="#" className="ImgSendMessage">
+          <em>Send</em>Message
+        </a>
       </div>
     </section>
+  );
+}
+
+export function renderFooter() {
+  const Sociallist = [
+    {
+      link: Social.email,
+      img: "Email",
+      content: "mvtnhan@gmail.com"
+    },
+    {
+      link: Social.skype,
+      img: "Skype",
+      content: "Skype"
+    },
+    {
+      link: Social.twitter,
+      img: "Twitter",
+      content: "Twitter"
+    },
+    {
+      link: Social.Linkedin,
+      img: "Linkedin",
+      content: "Linkedin"
+    }
+  ];
+  return (
+    <div className="Footer Container">
+      <p>Copyright © 2014 Web Craftsman. Contact Preferred Via:</p>
+      <div className="IconFooter">
+        {Sociallist.map(social => (
+          <>
+            {" "}
+            <a href={social.link}>
+              <Popup
+                className="TooltipCraftsman"
+                trigger={<Image className={social.img} />}
+                content={social.content}
+                position="top center"
+              />
+            </a>
+          </>
+        ))}
+      </div>
+    </div>
   );
 }
 
@@ -252,9 +252,10 @@ export default function Scraftsman() {
     <div className="Scraftsman">
       {renderHeader()}
       {renderIntro()}
-      {renderWorks()}
+      {renderMoreWorks()}
       {renderAbout()}
       {renderContact()}
+      {renderFooter()}
     </div>
   );
 }
