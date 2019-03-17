194a7f6b0d1694f3943dd4a80a08199c1ce64848
Nhan Mai <mvtnhan@gmail.com>
2019-03-17 17:43:22 +0700

    feat: change folder name

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Shopify from "./landing-pages/shopify/index.js";
-import Craftsman from "./landing-pages/craftsman/index.js";
+import Shopify from "./pages/shopify/index.js";
+import Craftsman from "./pages/craftsman/index.js";
 import Home from "./home.js";
 
-const App = () => (
-  <Router>
-    <div>
-      <Route path="/" exact component={Home} />
-      <Route path="/shopify/" component={Shopify} />
-      <Route path="/craftsman/" component={Craftsman} />
-    </div>
-  </Router>
-);
-
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Route path="/" exact component={Home} />
+        <Route path="/shopify/" component={Shopify} />
+        <Route path="/craftsman/" component={Craftsman} />
+      </div>
+    </Router>
+  );
+}
