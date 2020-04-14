e5327e6fb5c63833c001cce28a09d02e5ad250a6
NhanMai <mvtnhan@gmail.cpm>
2020-04-14 23:35:37 +0700

    Updates

---
+++ precache-manifest.ed200b24941004e4ead2379ede180923.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d846f2c9b5d28545c98",
+    "url": "./static/css/main.287459e2.chunk.css"
+  },
+  {
+    "revision": "2d846f2c9b5d28545c98",
+    "url": "./static/js/main.2d846f2c.chunk.js"
+  },
+  {
+    "revision": "a574c0719b602c44fefb",
+    "url": "./static/css/1.4aade9bd.chunk.css"
+  },
+  {
+    "revision": "a574c0719b602c44fefb",
+    "url": "./static/js/1.a574c071.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b985f22eb4e49246b09976165f6b5b41",
+    "url": "./static/media/laptop.b985f22e.png"
+  },
+  {
+    "revision": "b7bb024accf3ebfc577f9100c701c4d2",
+    "url": "./static/media/spapers.b7bb024a.jpg"
+  },
+  {
+    "revision": "786b68af84c10fece2f9d0b9f3644349",
+    "url": "./static/media/logo.786b68af.png"
+  },
+  {
+    "revision": "1baf0755a4753abcd5500e0c64da9b12",
+    "url": "./static/media/lets-host.1baf0755.png"
+  },
+  {
+    "revision": "15f35fdc8e8ddc77d53472979fd0c8cf",
+    "url": "./static/media/ff-venture-capital.15f35fdc.png"
+  },
+  {
+    "revision": "9c6ae226dc2febee87f79b042a5cd844",
+    "url": "./static/media/ravendb.9c6ae226.png"
+  },
+  {
+    "revision": "faeed2b7cd7579a61052cf73d4157fdb",
+    "url": "./static/media/cloudvps.faeed2b7.png"
+  },
+  {
+    "revision": "a8fc5a1e07b18fa7a97613b86e63c27a",
+    "url": "./static/media/host365.a8fc5a1e.png"
+  },
+  {
+    "revision": "d1828aae6f35ca9ba6590780f95d6a2c",
+    "url": "./static/media/medieval-wall.d1828aae.png"
+  },
+  {
+    "revision": "d280ab126c448829a74355d6df35a31d",
+    "url": "./static/media/about.d280ab12.png"
+  },
+  {
+    "revision": "609e5b51a5fc0922f927bbd0f02a5ab6",
+    "url": "./static/media/contact.609e5b51.png"
+  },
+  {
+    "revision": "683067eb8e2790b1c831d6dc9f8a2fa6",
+    "url": "./static/media/header-bg.683067eb.jpg"
+  },
+  {
+    "revision": "9d874b20c620b8979c86765fd1ca0766",
+    "url": "./static/media/ShopifySans--regular.9d874b20.woff2"
+  },
+  {
+    "revision": "504b4a6143b351d1e4789cc0d396d6df",
+    "url": "./static/media/ShopifySans--light.504b4a61.woff2"
+  },
+  {
+    "revision": "a10562f55aed7249c4d9dd2c1d54e620",
+    "url": "./static/media/ShopifySans--medium.a10562f5.woff2"
+  },
+  {
+    "revision": "5c6e4fe5d8680f1b834c192ee03c566d",
+    "url": "./static/media/ShopifySans--bold.5c6e4fe5.woff2"
+  },
+  {
+    "revision": "8225abe1120788dc2a1dd3b7d31f9946",
+    "url": "./static/media/ShopifySans--extrabold.8225abe1.woff2"
+  },
+  {
+    "revision": "1d0d60890f5a0d7bd1324ff2eb7c7e04",
+    "url": "./static/media/ShopifySans--black.1d0d6089.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "./static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "./static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "./static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "./static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "./static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "./static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "./static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "./static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "./static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "./static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "./static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "./static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "./static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "./static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "./static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "./static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "59e7e275c8156124c2d5276833f08bb5",
+    "url": "./static/media/intro.59e7e275.png"
+  },
+  {
+    "revision": "ad9bc5fdb79b918e1778fa2879e2e968",
+    "url": "./static/media/intro-central-banner.ad9bc5fd.png"
+  },
+  {
+    "revision": "f90059c0c803c433c31adab0a0255870",
+    "url": "./static/media/worksbg.f90059c0.jpg"
+  },
+  {
+    "revision": "96b47a2914a3e828bc82a28ad036fcaa",
+    "url": "./static/media/works.96b47a29.png"
+  },
+  {
+    "revision": "f6bd988c65affc6519cc890ed8bf0c8c",
+    "url": "./static/media/design-elements.f6bd988c.png"
+  },
+  {
+    "revision": "116a4bb855827a08ef709d4f617384ce",
+    "url": "./static/media/contactbg.116a4bb8.jpg"
+  },
+  {
+    "revision": "8840ca9fdf61f075ae9c694ece18e7f2",
+    "url": "./static/media/about-work-flow.8840ca9f.png"
+  },
+  {
+    "revision": "45316f121f35a82b3ee0d4554c402cf8",
+    "url": "./static/media/intro-small.45316f12.png"
+  },
+  {
+    "revision": "3cb787e72f6fa50684873ddb2718559f",
+    "url": "./static/media/Craftsman--black.3cb787e7.woff2"
+  },
+  {
+    "revision": "97899d8689e1e31a19ad3711c66e0bd8",
+    "url": "./static/media/Craftsman--bold.97899d86.woff2"
+  },
+  {
+    "revision": "b24564bb054858bc7f645676b3dc89ab",
+    "url": "./static/media/logoshopify.b24564bb.svg"
+  },
+  {
+    "revision": "d05ed12ffff8b6f292e575c0db89db98",
+    "url": "./static/media/feature-themes.d05ed12f.svg"
+  },
+  {
+    "revision": "ee33c5d6d0e5595014f0be8ed38ca963",
+    "url": "./static/media/feature-pricing.ee33c5d6.svg"
+  },
+  {
+    "revision": "35b8b5503512f40cb23ad11471035c8d",
+    "url": "./static/media/feature-trust.35b8b550.svg"
+  },
+  {
+    "revision": "c7f24b2ebc1d896da53f1a86bbe0fb8b",
+    "url": "./index.html"
+  }
+];
